737d7ffa4ca67cbdab099ca52262931bf2cae1d0
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-16 21:52:57 +0300

    add number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,55 @@
 import { Component } from 'react';
 import { Phonebook } from './Phonebook/Phonebook';
-// import { Contacts } from './Contacts/Contacts';
+import { Contacts } from './Contacts/Contacts';
 
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+  };
+
+  ChangeHandler = e => {
+    const {
+      target: { value, name },
+    } = e;
+    this.setState({ [name]: value });
   };
 
   SubmitHandler = e => {
     const {
       target: {
         elements: {
-          name: { value },
+          name: { value: NameValue },
+          number: { value: NumberValue },
         },
       },
     } = e;
     e.preventDefault();
-    this.setState(prevState => ({ contacts: [value, ...prevState.contacts] }));
+    this.setState(prevState => ({
+      contacts: [
+        { name: NameValue, number: NumberValue },
+        ...prevState.contacts,
+      ],
+    }));
     this.reset();
   };
-  ChangeHandler = ({ target: { value } }) => {
-    this.setState({ name: value });
-  };
+
   reset = () => {
-    this.setState({ name: '' });
+    this.setState({ name: '', number: '' });
   };
 
   render() {
-    const { name } = this.state;
+    const { contacts, name, number } = this.state;
     return (
       <>
         <Phonebook
           onSubmitClick={this.SubmitHandler}
-          value={name}
+          nameValue={name}
+          numberValue={number}
           ChangeHandler={this.ChangeHandler}
         />
-        {/* <Contacts /> */}
+        <Contacts contacts={contacts} />
       </>
     );
   }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,4 +1,9 @@
-export function Phonebook({ onSubmitClick, value, ChangeHandler }) {
+export function Phonebook({
+  onSubmitClick,
+  nameValue,
+  numberValue,
+  ChangeHandler,
+}) {
   return (
     <form onSubmit={onSubmitClick}>
       <label htmlFor="name">Name</label>
@@ -9,7 +14,18 @@ export function Phonebook({ onSubmitClick, value, ChangeHandler }) {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        value={value}
+        value={nameValue}
+        onChange={ChangeHandler}
+      />
+      <label htmlFor="tel">Number</label>
+      <input
+        id="tel"
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={numberValue}
         onChange={ChangeHandler}
       />
       <button type="submit">Add contact</button>
