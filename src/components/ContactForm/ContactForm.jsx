191b2c5505186f1f0df3e0cdcd60f316f9599f27
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-21 19:19:33 +0300

    start Formic

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,27 +1,44 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const Schema = Yup.object().shape({
+  name: Yup.string().required('Required'),
+  number: Yup.number().required('Required'),
+});
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+const onSubmit = (values, { resetForm }) => {
+  console.log(values);
+  resetForm({
+    values: {
+      name: '',
+      number: '',
+    },
+  });
+};
+
 export function ContactForm(props) {
   return (
-    <>
-      <form onSubmit={props.submitHandler}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <label htmlFor="tel">Number</label>
-        <input
-          id="tel"
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <button type="submit">Add contact</button>
-      </form>
-    </>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={Schema}
+      onSubmit={onSubmit}
+    >
+      {() => (
+        <Form>
+          <label htmlFor="name">Name</label>
+          <Field id="name" name="name" />
+          <ErrorMessage name="name">{msg => <div>{msg}</div>}</ErrorMessage>
+          <label htmlFor="number">Number</label>
+          <Field id="number" name="number" />
+          <ErrorMessage name="number">{msg => <div>{msg}</div>}</ErrorMessage>
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
