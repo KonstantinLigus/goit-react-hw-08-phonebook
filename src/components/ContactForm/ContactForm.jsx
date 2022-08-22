223251c26b79f92be91d2dfd383bb8f8f2ad7757
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-22 06:26:38 +0300

    complete validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,8 +2,18 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const Schema = Yup.object().shape({
-  name: Yup.string().required('Required'),
-  number: Yup.number().required('Required'),
+  name: Yup.string()
+    .required('Required')
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    ),
+  number: Yup.string()
+    .required('Required')
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    ),
 });
 
 const initialValues = {
@@ -23,6 +33,29 @@ const onSubmit = (values, { resetForm }) => {
 
 export function ContactForm(props) {
   return (
+    // <form
+    //   onSubmit={e => {
+    //     e.preventDefault();
+    //     console.log(e.target.elements.name.value);
+    //     console.log(e.target.elements.number.value);
+    //   }}
+    // >
+    //   <input
+    //     type="text"
+    //     name="name"
+    //     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+    //     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    //     required
+    //   />
+    //   <input
+    //     type="tel"
+    //     name="number"
+    //     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+    //     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+    //     required
+    //   />
+    //   <button type="submit">Submit</button>
+    // </form>
     <Formik
       initialValues={initialValues}
       validationSchema={Schema}
