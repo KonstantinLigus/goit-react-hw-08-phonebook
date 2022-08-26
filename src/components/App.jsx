c725bf0d08759bfa9b2ec328b268900324239a39
Konstantin Ligus <kostya1989com@gmail.com>
2022-08-26 10:01:29 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,19 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   changeFilterHandler = e => {
     const {
       target: { value, name },
